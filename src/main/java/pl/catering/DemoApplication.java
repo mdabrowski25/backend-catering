066f6d886181d0b4a018caffba0c3a1699bcdfea
mdabrowski25 <m.dabrowski97@o2.pl>
2021-01-28 13:00:56 +0100

    cors

--- src/main/java/pl/catering/DemoApplication.java
+++ src/main/java/pl/catering/DemoApplication.java
@@ -13,14 +13,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
-    }
 }
