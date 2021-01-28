63b0db40a4daffa79a3c3707a7846564c94ffe85
mdabrowski25 <m.dabrowski97@o2.pl>
2021-01-28 12:24:55 +0100

    cors

--- src/main/java/pl/catering/DemoApplication.java
+++ src/main/java/pl/catering/DemoApplication.java
@@ -2,6 +2,9 @@ package pl.catering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,18 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(
+                        "http://localhost:8080",
+                        "http://localhost:5555",
+                        "http://localhost:4200",
+                        "https://frontend-catering-s-krol.netlify.app")
+                        .allowedMethods("POST", "PUT", "GET");
+            }
+        };
+    }
 }
