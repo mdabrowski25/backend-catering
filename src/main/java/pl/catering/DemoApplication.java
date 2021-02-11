36882b426162fb6713e8a11faa8690fc45a5860d
mdabrowski25 <m.dabrowski97@o2.pl>
2021-02-11 17:52:11 +0100

    localhost fix

--- src/main/java/pl/catering/DemoApplication.java
+++ src/main/java/pl/catering/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,4 +12,20 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(
+                        "http://localhost:8080",
+                        "http://localhost:5555",
+                        "http://localhost:4200",
+                        "https://frontend-catering-s-krol.netlify.app")
+                        .allowedMethods("POST", "PUT", "GET");
+            }
+        };
+    }
+
 }
